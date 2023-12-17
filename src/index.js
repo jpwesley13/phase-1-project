1e9390554c9f26ab9264becf4d85837a57f1aae6
Jared Patterson <jpwesley34@gmail.com>
2023-12-16 18:50:30 -0800

    Does a bit of troubleshooting.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const urlBar = 'https://pokeapi.co/api/v2/pokemon/'
 const pokeBar = document.querySelector('#pokemon-bar');
 const pokeDetails = document.querySelector('#pokemon-details')
+const pokeImage = document.querySelector('#pokemon-image')
 function randomPokemon(gen1, gen9){
     return Math.floor(Math.random() * (gen9 - gen1 + 1) + gen1)
 };
@@ -35,23 +36,25 @@ function seeMore(id){
 
 
 //                  Data Displays
-function pokeThumbnails(pokemon){
-    pokemon.forEach(monster => {
-        const pokePic = document.createElement('img');
-        pokePic.classList.add('pokemon-thumbnail');
-        pokePic.dataset.id = monster.id;
-        pokePic.src = monster.sprites.other['official-artwork'].front_default;
+function pokeThumbnails(pokemon) {
+  pokemon.forEach(monster => {
+    const pokePic = document.createElement('img');
+    pokePic.classList.add('pokemon-thumbnail');
+    pokePic.id = monster.id;
+    pokePic.defaultPoke = monster.sprites.other['official-artwork'].front_default;
+    pokePic.shinyPoke = monster.sprites.other['official-artwork'].front_shiny;
+    pokePic.src = pokePic.defaultPoke;
 
-        pokeBar.appendChild(pokePic);
-    });
-};
+    pokeBar.appendChild(pokePic);
+  });
+}
 
 function mainCard(id){
     pokeDetails.innerHTML = `
   <div class="card" id="pokemon-details">
     <h2>${id.name}</h2>
     <img src="${id.sprites.other['official-artwork'].front_default}"
-    class="full-image" />
+    class="full-image" id="pokemon-image"/>
     <p>Weight: ${id.weight / 10} kilogram(s)</p>
     <p>Height: ${id.height / 10} meter(s)</p>
     
@@ -62,7 +65,7 @@ function mainCard(id){
 //                  Event Listeners
 pokeBar.addEventListener('click', function(e) {
     if(e.target.className === 'pokemon-thumbnail'){
-        const pokemonID = e.target.dataset.id;
+        const pokemonID = e.target.id;
         pokeDetails.innerHTML = '';
         seeMore(pokemonID);
     };
@@ -74,5 +77,18 @@ document.addEventListener('keydown', function(e) {
         fetchPokemon();
     };
 });
+
+pokeDetails.addEventListener('click', function(e){
+    if(e.target.tagName === 'IMG') {
+        const baseUrl = pokeImage.src;
+        const shinyUrl = e.target.shinyPoke;
+
+        if(baseUrl === shinyUrl){
+            e.target.src = e.target.defaultPoke
+        } else {
+            e.target.src = shinyUrl
+        }
+    }
+})
 
 fetchPokemon();
